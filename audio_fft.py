d21b89e6069b93d8f090438037ab2ce5e4f9fdb0
joshie <89046683+JoshW17@users.noreply.github.com>
2024-03-25 17:04:04 -0400

    Update audio_fft.py
    fix then n values of the shifted fft graph

--- audio_fft.py
+++ audio_fft.py
@@ -15,9 +15,11 @@ plt.title('Audio Wave in Time')
 # Plot FFT of audio data shifted
 plt.figure(2)
 
+N = len(data)
+x = np.linspace(-N/2, (N/2)-1, N)
 fft_result = np.fft.fft(data) # FFT fo audio data
 
-plt.plot(np.fft.fftshift(abs(fft_result))) # Take magnitude with abs() and shift data with fftshift()
+plt.plot(x, np.fft.fftshift(abs(fft_result))) # Take magnitude with abs() and shift data with fftshift()
 plt.xlabel('Sample Index (N)')
 plt.ylabel('Magnitude')
 plt.title('FFT shifted')
