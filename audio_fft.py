66f4524a5387bce9fde426abad85daa8a75a2db8
joshie <89046683+JoshW17@users.noreply.github.com>
2024-03-25 16:38:19 -0400

    Create audio_fft.py
    plot audio data in time then take the FFT and plot the magnitude and shift the data

---
+++ audio_fft.py
@@ -0,0 +1,24 @@
+import soundfile as sf
+import numpy as np
+import matplotlib.pyplot as plt
+
+filename = input('Input filename (please use full path): ')
+data, fs = sf.read(filename)
+
+# Plot raw audio data
+plt.figure(1)
+plt.plot(data)
+plt.xlabel('Sample Index (N)')
+plt.ylabel('Amplitude')
+plt.title('Audio Wave in Time')
+
+# Plot FFT of audio data shifted
+plt.figure(2)
+
+fft_result = np.fft.fft(data) # FFT fo audio data
+
+plt.plot(np.fft.fftshift(abs(fft_result))) # Take magnitude with abs() and shift data with fftshift()
+plt.xlabel('Sample Index (N)')
+plt.ylabel('Magnitude')
+plt.title('FFT shifted')
+plt.show()
