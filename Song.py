b414d221129c5d1285e354554677ac5bddb69d82
Christian <c.ship.johnson@gmail.com>
2024-03-30 14:35:57 -0400

    Added Song class and main.py

---
+++ Song.py
@@ -0,0 +1,6 @@
+
+
+
+class Song():
+    def __init__(self):
+        print("This is a song")
---
+++ main.py
@@ -0,0 +1,9 @@
+from Song import Song
+
+
+def main():
+    test_song=Song()
+
+
+if __name__ == "__main__":
+    main()
