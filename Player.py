312ab566fceed65aeeaff4fc6c68196b8fbbf504
Daniel Nusraty <113135674+DANIELN09@users.noreply.github.com>
2024-04-03 21:55:00 -0400

    practicing with widgets

--- Player.py
+++ Player.py
@@ -1,24 +1,19 @@
 import customtkinter as ctk
-import pyglet
 
 class Player():
     """Media player to handle MP3 files and audio output"""
     
-
-
 class MusicPlayer():
     """GUI Media Player to allow user interface"""
     def __init__(self, master):
-        super().__init__()
         self.master=master
-        self.master.geometry("400x900")
-        self.master.title("Music Player")
         
-        # Create a frame to hold the widgets (Dan)
+        # Create a frame to hold the widgets 
         self.frame = ctk.CTkFrame(self.master)
-        self.frame.pack(padx=20, pady=20)
+        self.frame.pack(padx=(0,1400), pady=(500,0))
         
-        self.frame_top=ctk.CTkFrame(self.frame, height=40)
+        # The second smaller frame 
+        self.frame_top=ctk.CTkFrame(self.frame, fg_color="#999999",width=500,height=1500)
         self.frame_top.pack(side="top", expand=True, fill="x")
     
     def play_music(self):
@@ -27,6 +22,8 @@ class MusicPlayer():
     
 if __name__ == "__main__":
     root=ctk.CTk()
+    root.geometry("400x900")
+    root.title("Music Player")   
 
     MusicPlayer(root)
 
