b3d4bdc0b15fe6fb6a2ffa71ad4bf852c6473a99
Daniel Nusraty <113135674+DANIELN09@users.noreply.github.com>
2024-04-07 21:48:52 -0400

    Added several widgets to gui

--- Player.py
+++ Player.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+import tkinter as tk
+from tkinter import ttk
 
 class MusicPlayer:
     """GUI Media Player to allow user interface"""
@@ -9,13 +11,48 @@ class MusicPlayer:
         self.player_frame = ctk.CTkFrame(self.master, fg_color="#999999")
         self.player_frame.pack(side="left" ,padx=(10), pady=(10), ipady=(10), ipadx=(10),expand=True, fill="both")
         
-        # The second smaller frame 
-        self.frame_album_cover=ctk.CTkFrame(self.player_frame, fg_color="#5a4")
-        self.frame_album_cover.pack(side="top", expand=True, fill="x", padx=(10))
+        # The title of the music player 
+        label = ctk.CTkLabel(self.player_frame, text="Music Player", text_color='black')
+        label.pack(padx=0,pady=0)
 
+        # The frame to hold the album cover 
+        self.frame_album_cover=ctk.CTkFrame(self.player_frame, fg_color="#5a4")
+        self.frame_album_cover.pack(side="top", fill="x",padx=(10))
+
+        # The frame to hold the playlist 
         self.playlist_frame=ctk.CTkFrame(self.master, fg_color="#999999")
         self.playlist_frame.pack(side="right", padx=(10), pady=(10), ipady=(10), ipadx=(10), expand=True, fill="both")
-    
+
+        # The title of the playlist
+        label = ctk.CTkLabel(self.playlist_frame, text="Recommended Songs", text_color='black')
+        label.pack(padx=0,pady=0)
+
+        # The grid for the playlist songs 
+        playlist = ttk.Treeview(self.playlist_frame, columns=('Song', 'Artist'), show='headings')
+        playlist.heading('Song', text='Song')
+        playlist.heading('Artist', text='Artist')
+        data = [('I Want It That Way', 'Backstreet Boys'), ('Never Gonna Give You Up', 'Rick Astley'), ('All Star', 'Smash Mouth')]
+        for Song, Artist in data:
+            playlist.insert('', tk.END, values=(Song, Artist))
+        playlist.pack(expand=True, fill='both')
+
+        # Scrollbar for the playlist
+        # tree=ttk.Treeview(playlist)
+        # scrollbar = ttk.Scrollbar(playlist, orient='vertical', command=tree.yview)
+        # tree.configure(yscrollcommand=scrollbar.set)
+        # tree.pack(side='left', fill='both', expand=True)
+        # scrollbar.pack(side='right', fill='y')
+
+        # Forward button
+        forward = ctk.CTkButton(master=self.player_frame, text=" ---> ")
+        forward.pack(padx=20, pady=10)
+        # Pause button
+        pause = ctk.CTkButton(master=self.player_frame, text=" | | ")
+        pause.pack(padx=20, pady=10)
+        # Back button 
+        back = ctk.CTkButton(master=self.player_frame, text=" <--- ")
+        back.pack(padx=20, pady=10)
+        
     def play_music(self):
         # Add your logic here to play the selected music file
         print("Playing music...")
